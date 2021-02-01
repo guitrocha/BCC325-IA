f68587101bd46c5e1bc92d68ed6c966d1fee726c
Guilherme Ferreira Rocha <45642410+guitrocha@users.noreply.github.com>
2021-02-01 19:22:34 -0300

    GIT CLONE

--- definitions.py
+++ definitions.py
@@ -1,39 +1,21 @@
 class Environment:
     """
-        Implements the interface for an Environment
+    This class implements and interface for an Environment
     """
-    def initial_percepts ( self ):
-        """
-        Returns the environment initial percepts
-        Raises:
-            NotImplementedError: If the method is not implemented or not overridden.
-        """
-        raise NotImplementedError ('initial_percepts')
-    
-    def signal ( self , action ) :
-        """
-        Returns the environment percepts after action is exexuted
-        Raises:
-            NotImplementedError: If the method is not implemented or not overridden.
-        """
-        raise NotImplementedError ('signal')
+    def initial_percepts(self):
+        raise NotImplementedError('initial_percepts')
+
+    def signal(self, action):
+        raise NotImplementedError('signal')
+
 
 class Agent:
     """
-        Implements the interface for an Agent
+    This class implements an interface for an agent
     """
     def __init__(self, env):
-        """
-        Constructor for the agent class
-        Args:
-            env: a reference to an environment
-        """
         self.env = env
 
     def act(self):
-        """
-        Defines the agent action
-        Raises:
-            NotImplementedError: If the method is not implemented or not overridden.
-        """
         raise NotImplementedError('act')
+    
