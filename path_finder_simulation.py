2532817183be1a984d1b47f8d2fb2cf82e1ea8d3
Guilherme Ferreira Rocha <45642410+guitrocha@users.noreply.github.com>
2021-02-22 12:30:08 -0300

    Complete Path Finder
    Rand+IDA+BFS+DFS+AStar+BB+Greedy

--- path_finder_simulation.py
+++ path_finder_simulation.py
@@ -1,14 +1,16 @@
 from room import Room
-from path_finder_agents import RandAgent, DFSAgent, BFSAgent
+from path_finder_agents import RandAgent, IterativeDeepeningAgent, BFSAgent, DFSAgent, AStarAgent, BBAgent, GreedyAgent
 
 # env = Room(room=[[0, 0, 1], [0, 0, 1], [0, 0, 0]], target=[2, 2])
 env = Room(prob=0.3, n=25, plot_on=True)
 # env = Room(prob=0.1, n=5, plot_on=True)
 # agent = RandAgent(env)
 # agent = BFSAgent(env)
-agent = DFSAgent(env)
-# agent = AStarAgent(env)
+# agent = DFSAgent(env)
+agent = AStarAgent(env)
 # agent = GreedyAgent(env)
+# agent = BBAgent(env, bound=20)
+# agent = IterativeDeepeningAgent(env)
 
 agent.run()
 input('Press ENTER to get out of here')
